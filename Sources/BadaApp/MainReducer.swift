d667e68617431bf943077bd7a40efe6019764ba4
SeungYeop Yeom <dev.yeom@gmail.com>
2024-09-16 17:42:05 +0900

    Implement the splash view

--- Sources/BadaApp/MainReducer.swift
+++ Sources/BadaApp/MainReducer.swift
@@ -3,17 +3,26 @@ import BadaCore
 struct MainReducer: Reducer {
     enum Action: Sendable {
         case load
+        case setIsLoaded(Bool)
     }
 
     struct State: Sendable, Equatable {
         var isLoaded: Bool = false
-        init() { }
     }
 
     func reduce(state: inout State, action: Action) -> AnyEffect<Action> {
         switch action {
         case .load:
-            state.isLoaded = true
+            return .single {
+                do {
+                    try await Task.sleep(for: .seconds(1))
+                    return .setIsLoaded(true)
+                } catch {
+                    return .setIsLoaded(false)
+                }
+            }
+        case let .setIsLoaded(isLoaded):
+            state.isLoaded = isLoaded
             return .none
         }
     }
