5c00ff7373079add8af10fc207c74b16884ed5f2
Seungyeop Yeom <dev.yeom@gmail.com>
2024-09-18 00:33:02 +0900

    Implement shortcuts user interface in the home view (#19)

--- Sources/BadaMacApp/HomeView.swift
+++ Sources/BadaMacApp/HomeView.swift
@@ -17,9 +17,73 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             ScrollView(.vertical) {
-
+                VStack(spacing: 16) {
+                    HStack(spacing: 16) {
+                        StatusView(
+                            icon: .number,
+                            title: "Logs",
+                            value: "100",
+                            background: .tertiary
+                        )
+                        StatusView(
+                            icon: .personTextRectangle,
+                            title: "License",
+                            value: "Open water",
+                            background: .cyan
+                        )
+                    }
+                    HStack(spacing: 16) {
+                        StatusView(
+                            icon: .stopwatch,
+                            title: "Total time",
+                            value: "323h 40m",
+                            background: .orange
+                        )
+                        StatusView(
+                            icon: .calendar,
+                            title: "Last dive",
+                            value: "30d ago",
+                            background: .mint
+                        )
+                    }
+                }
+                .padding(16)
             }
             .navigationTitle(L10n.Home.title)
         }
     }
+}
+
+extension HomeView {
+    private struct StatusView<Background>: View where Background: ShapeStyle {
+        var icon: SystemImage
+        var title: String
+        var value: String
+        var background: Background
+
+        var body: some View {
+            VStack(spacing: 16) {
+                HStack(spacing: 4) {
+                    Image(systemImage: icon)
+                    Text(title)
+                    Spacer()
+                }
+                .font(.headline)
+                HStack(spacing: 0) {
+                    Spacer()
+                    Text(value)
+                        .font(.system(.title3, design: .rounded))
+                }
+            }
+            .padding(16)
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(background)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
 }
