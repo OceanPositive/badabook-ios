e65a5b4a13a5b7573f01c6b1c5bb0e11a97e81c5
Seungyeop Yeom <dev.yeom@gmail.com>
2024-09-17 11:05:33 +0900

    Apache License, Version 2.0 (#16)

--- Tests/BadaDataTests/DiveLogRepositoryTests.swift
+++ Tests/BadaDataTests/DiveLogRepositoryTests.swift
@@ -1,3 +1,10 @@
+//
+//  BadaBook
+//  Apache License, Version 2.0
+//
+//  Copyright (c) 2024 Seungyeop Yeom ( https://github.com/OceanPositive ).
+//
+
 import XCTest
 import BadaDomain
 @testable import BadaData
