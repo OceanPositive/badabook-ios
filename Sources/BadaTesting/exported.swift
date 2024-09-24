5b3572224bb1504849107b154cd8cb078418a73d
SeungYeop Yeom <dev.yeom@gmail.com>
2024-09-24 21:54:21 +0900

    Apply `OneWayTesting` to the store test cases

---
+++ Sources/BadaTesting/exported.swift
@@ -0,0 +1,9 @@
+//
+//  BadaBook
+//  Apache License, Version 2.0
+//
+//  Copyright (c) 2024 Seungyeop Yeom ( https://github.com/OceanPositive ).
+//
+
+@_exported import Testing
+@_exported import OneWayTesting
