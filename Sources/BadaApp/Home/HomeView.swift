c835b722aa2906b8200ec3ca10b62adb46c67c6f
Seungyeop Yeom <dev.yeom@gmail.com>
2025-01-18 15:41:06 +0900

    Add an action to the home section header (#49)

--- Sources/BadaApp/Home/HomeView.swift
+++ Sources/BadaApp/Home/HomeView.swift
@@ -9,16 +9,23 @@ import BadaCore
 import BadaUI
 
 struct HomeView: View {
+    @ObservedObject private var navigationStore = NavigationStore.shared
     @StateObject private var store = ViewStore(
         reducer: HomeReducer(),
         state: HomeReducer.State()
     )
 
     var body: some View {
-        NavigationStack {
+        NavigationStack(
+            path: navigationStore.binding(
+                \.homePaths,
+                 send: { .setHomePaths($0) })
+        ) {
             ScrollView(.vertical) {
-                VStack(alignment: .leading, spacing: 16) {
-                    SectionHeader(title: "Bio")
+                VStack(spacing: 16) {
+                    SectionHeader(title: "Bio") {
+                        // TODO: Move to the bio edit page
+                    }
                     VStack(spacing: 16) {
                         InfoRow(
                             title: "License",
@@ -33,7 +40,9 @@ struct HomeView: View {
                     .padding(16)
                     .background(.background.quaternary)
                     .clipShape(.rect(cornerRadius: 8))
-                    SectionHeader(title: "Summary")
+                    SectionHeader(title: "Summary") {
+                        navigationStore.send(.logbook(.root))
+                    }
                     VStack(spacing: 16) {
                         InfoRow(
                             title: "Logs",
@@ -63,6 +72,8 @@ struct HomeView: View {
             }
             .background(.background.secondary)
             .navigationTitle(L10n.Home.title)
+            .navigationDestination(for: NavigationState.HomePath.self) { path in
+            }
             .onAppear(perform: onAppear)
         }
     }
@@ -75,12 +86,23 @@ struct HomeView: View {
 extension HomeView {
     private struct SectionHeader: View {
         let title: String
+        let action: () -> Void
 
         var body: some View {
-            Text(title)
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundStyle(.foreground)
+            HStack(spacing: 0) {
+                Text(title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.foreground)
+                Spacer()
+                Image(systemImage: .chevronRight)
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                action()
+            }
         }
     }
 
