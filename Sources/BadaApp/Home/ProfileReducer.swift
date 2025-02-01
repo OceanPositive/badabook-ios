3389f12baf93a51810356029449c6837a2c3cd0b
Seungyeop Yeom <dev.yeom@gmail.com>
2025-02-01 16:14:12 +0900

    Implement the profile view (#54)

--- Sources/BadaApp/Home/ProfileReducer.swift
+++ Sources/BadaApp/Home/ProfileReducer.swift
@@ -6,14 +6,22 @@
 //
 
 import BadaCore
+import BadaDomain
 
 struct ProfileReducer: Reducer {
     enum Action: Sendable {
         case load
         case save
+        case setFirstName(String)
+        case setLastName(String)
+        case setDateOfBirth(Date)
     }
 
     struct State: Sendable, Equatable {
+        var firstName: String = ""
+        var lastName: String = ""
+        var dateOfBirth: Date = Date(timeIntervalSince1970: 0)
+        var certifications: [Certification] = []
     }
 
     func reduce(state: inout State, action: Action) -> AnyEffect<Action> {
@@ -21,6 +29,15 @@ struct ProfileReducer: Reducer {
         case .load:
             return .none
         case .save:
+            return .none
+        case let .setFirstName(firstName):
+            state.firstName = firstName
+            return .none
+        case let .setLastName(lastName):
+            state.lastName = lastName
+            return .none
+        case let .setDateOfBirth(dateOfBirth):
+            state.dateOfBirth = dateOfBirth
             return .none
         }
     }
