9649b161338b3fca68e15c4e1d49a19af6bd77f4
Seungyeop Yeom <dev.yeom@gmail.com>
2025-03-22 15:18:25 +0900

    Implement the certification list view (#60)

--- Sources/BadaApp/Home/ProfileReducer.swift
+++ Sources/BadaApp/Home/ProfileReducer.swift
@@ -14,6 +14,7 @@ struct ProfileReducer: Reducer {
         case save
         case setName(String)
         case setDateOfBirth(Date)
+        case setCertifications([Certification])
         case setIsCertificationAddSheetPresenting(Bool)
     }
 
@@ -24,10 +25,12 @@ struct ProfileReducer: Reducer {
         var isCertificationAddSheetPresenting: Bool = false
     }
 
+    @UseCase private var getCertificationsUseCase: GetCertificationsUseCase
+
     func reduce(state: inout State, action: Action) -> AnyEffect<Action> {
         switch action {
         case .load:
-            return .none
+            return .single { await executeGetCertificationsUseCase() }
         case .save:
             return .none
         case let .setName(name):
@@ -36,9 +39,23 @@ struct ProfileReducer: Reducer {
         case let .setDateOfBirth(dateOfBirth):
             state.dateOfBirth = dateOfBirth
             return .none
+        case let .setCertifications(certifications):
+            state.certifications = certifications
+            return .none
         case let .setIsCertificationAddSheetPresenting(isCertificationAddSheetPresenting):
             state.isCertificationAddSheetPresenting = isCertificationAddSheetPresenting
             return .none
+        }
+    }
+
+    private func executeGetCertificationsUseCase() async -> Action {
+        let result = await getCertificationsUseCase.execute()
+        switch result {
+        case let .success(certifications):
+            let sorted = certifications.sorted(by: { $0.date > $1.date })
+            return .setCertifications(sorted)
+        case .failure:
+            return .setCertifications([])
         }
     }
 }
