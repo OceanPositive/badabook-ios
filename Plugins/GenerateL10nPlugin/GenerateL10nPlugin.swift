e769b28d0ba9b9731f8704fb0952268ad91c36f6
Seungyeop Yeom <dev.yeom@gmail.com>
2025-08-10 13:06:59 +0900

    Update copyright year (#86)

--- Plugins/GenerateL10nPlugin/GenerateL10nPlugin.swift
+++ Plugins/GenerateL10nPlugin/GenerateL10nPlugin.swift
@@ -1,8 +1,8 @@
 //
-//  BadaBook
+//  Badabook
 //  Apache License, Version 2.0
 //
-//  Copyright (c) 2024 Seungyeop Yeom ( https://github.com/OceanPositive ).
+//  Copyright (c) 2025 Seungyeop Yeom ( https://github.com/OceanPositive ).
 //
 
 import PackagePlugin
